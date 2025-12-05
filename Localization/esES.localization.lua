f75ccfaf83d5097e86574069057a3f45acdc186c
Gogo1951 <55365231+Gogo1951@users.noreply.github.com>
2025-12-05 17:31:13 -0600

    Add files via upload

---
+++ Localization/esES.localization.lua
@@ -0,0 +1,157 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("GDKPd", "esES")
+if not L then return end
+
+L["Allow multiple simultanous auctions"] = "Permitir multiples subastas simultáneas" -- Needs review
+-- L["Always show the \"Hide\" button on bid frames"] = "Always show the \"Hide\" button on bid frames"
+L["Amount"] = "Cantidad"
+L["Anchor balance window to status window"] = "Anclar ventana de balance a la ventana de estado"
+L["Announce auction start to raid warning"] = "Anunciar comienzo de subasta por aviso de banda"
+L["Announce & auto-auction"] = "Anuncio & auto-subasta" -- Needs review
+L["Announce bids to raid warning"] = "Anunciar pujas por aviso de raid"
+L["Announce loot"] = "Anunciar loot"
+L["Announce the current pot amount after each auction"] = "Anunciar cantidad del bote actual tras cada subasta"
+-- L["Any money subtracted from raid members is added to the pot and vice versa"] = "Any money subtracted from raid members is added to the pot and vice versa"
+L["Appearance options"] = "Opciones de apariencia"
+-- L["Are you sure you want to mail %s gold to player %s?"] = "Are you sure you want to mail %s gold to player %s?"
+L["Auction bid timeout refresh"] = "Refrescar tiempo de pujas en subasta"
+L["Auction history"] = "Historial de subastas"
+L["Auction note: %s"] = "Nota de subasta: %s" -- Needs review
+L["Auction timeout"] = "Tiempo para la subasta" -- Needs review
+L["Auto-award loot to winner"] = "Auto-entregar loot al ganador"
+L["Auto bid"] = "Auto puja"
+L["Award loot to Master Looter when auto-auctioning"] = "Entregar loot al Maestro Despojador cuando se auto-subaste"
+L["Balance"] = "Balance"
+L["Behaviour options"] = "Opciones de comportamiento"
+L["Bid"] = "Puja"
+L["Bid button re-enable delay"] = "Reactivar retraso del boton de puja" -- Needs review
+L["Broadcast rules"] = "Reglas de anuncio"
+L["Cancel auction"] = "Cancelar subasta"
+L["Cannot start auction without Master Looter privileges."] = "No se puede comenzar una subasta sin privilegios de Maestro Despojador."
+-- L[" |cffaa0000(Distribute: %dg)|r"] = " |cffaa0000(Distribute: %dg)|r"
+L["Control panel scale"] = "Escala del panel de control"
+-- L["Countdown timer announce interval"] = "Countdown timer announce interval"
+L["Current bid: "] = "Puja actual:"
+L["Distribute"] = "Distribuir"
+L["Do not announce"] = "No anunciar"
+L["Do you want to save your pot or reset without saving? You can also add a note to the pot."] = "¿Quieres registrar tu bote o reiniciar sin salvar? También puedes añadir una nota al bote." -- Needs review
+--[==[ L[ [=[Due to the changes to the addon message system implemented in patch 4.2, GDKPd is no longer able to communicate using its old version checking standard.
+Thus, this version of GDKPd will only be able to send and receive version checks from and to versions 1.2.0 and above of GDKPd.
+While all other functionalities of GDKPd should still be compatible with previous versions, we |cffff0000strongly recommend updating GDKPd to version 1.2.0 or above|r.]=] ] = [=[Due to the changes to the addon message system implemented in patch 4.2, GDKPd is no longer able to communicate using its old version checking standard.
+Thus, this version of GDKPd will only be able to send and receive version checks from and to versions 1.2.0 and above of GDKPd.
+While all other functionalities of GDKPd should still be compatible with previous versions, we |cffff0000strongly recommend updating GDKPd to version 1.2.0 or above|r.]=] ]==]
+L["Enable"] = "Permitir"
+L["Enter the amount you want to add to player %s:"] = "Introduce la cantidad que quieres añadir al jugador %s:"
+L["Enter the amount you want to add to the pot:"] = "Introduce la cantidad que quieres añadir al bote:"
+L["Enter the amount you want to subtract from player %s:"] = "Introduce la cantidad que quieres sustraer al jugador %s:"
+L["Enter the amount you want to subtract from the pot:"] = "Introduce la cantidad que quieres sustraer al bote:"
+L["Enter the maximum amount of money you want to bid on %s:"] = "Introduce la cantidad máxima de dinero que quieres pujar en %s:"
+L["Export"] = "Exportar"
+L["Frame alpha"] = "Alfa de ventanas" -- Needs review
+L["Frame scale"] = "Escala de ventanas"
+L["GDKPd auction history"] = "Historial de subastas GDKPd"
+L["GDKPd auction history for %s"] = "Historial de subastas GDKPd por %s"
+--[==[ L[ [=[GDKPd: Drag to move
+/gdkpd and check "Lock" to hide]=] ] = [=[GDKPd: Drag to move
+/gdkpd and check "Lock" to hide]=] ]==]
+L["Loot dropped: "] = "Loot caído:"
+L["GDKPd: No detailed data available"] = "GDKPd: Sin datos disponibles" -- Needs review
+L["This raid uses GDKPd to faciliate its GDKP bidding process. While you can bid on items without having GDKPd installed, installing it provides you with a GUI bidding panel, auto bidding functions, auction timers, chat filtering and more!"] = "Esta raid usa GDKPd para facilitar sus procesos de subasta GDKP. Aunque puedas pujar en objetos sin tener GDKPd instalado, instalarlo te provee de un panel GUI de pujas, funciones de auto pujas, cronómetros de subasta, filtros de chat ¡y más!"
+-- L["GDKPd version %s. Packaged %s."] = "GDKPd version %s. Packaged %s."
+L["Your version of GDKPd is outdated and no longer compatible with the raid leader's in one or more functionalities. In order to ensure smooth performance, please update GDKPd."] = "Tu versión de GDKPd está desactualizada y no es compatible con la del líder de banda en una o más funciones. Para asegurar un funcionamiento preciso, por favor, actualiza GDKP."
+L["Your version of GDKPd is slightly outdated compared to the raid leader's. Full compability should be possible, however, you might want to take some time and update GDKPd."] = "Tu versión de GDKPd está ligeramente desactualizada comparada con la del líder de banda. Compatibilidad completa podría ser posible, sin embargo convendría que busques algo de tiempo y actualices GDKPd."
+L["Hide"] = "Ocultar"
+L["Hide 'Auction cancelled' announcements"] = "Ocultar anuncios de 'Subasta cancelada'"
+L["Hide 'Auction cancelled' announcements from raid warning"] = "Ocultar anuncios de 'Subasta cancelada' de los avisos de raid"
+L["Hide 'Auction finished' announcements"] = "Ocultar anuncios de 'Subasta terminada'"
+L["Hide 'Bidding starts' announcements"] = "Ocultar anuncios de 'Comienzo de pujas'"
+L["Hide 'Bidding starts' announcements from raid warning"] = "Ocultar anuncios de 'Comienzo de pujas' de los avisos de raid"
+L["Hide chat messages"] = "Ocultar mensajes de chat"
+L["Hide 'Current pot:' announcements"] = "Ocultar anuncios de 'Bote actual:'"
+L["Hide frames in combat"] = "Ocultar ventanas en combate"
+L["Hide history window"] = "Ocultar ventana de historia"
+L["Hide 'New highest bidder' announcements"] = "Ocultar anuncios de 'Nueva puja mas alta'"
+L["Hide players' bid messages"] = "Ocultar mensajes de puja de jugadores"
+L["Hide status and balance windows"] = "Ocultar ventanas de estado y balance"
+L["Hide 'Time remaining' announcements"] = "Ocultar anuncios de 'Tiempo disponible'"
+L["Hide version check window"] = "Ocultar ventana de comprobación de version"
+L["Highest bidder: %s"] = "Mayor puja: %s"
+L["History"] = "Historial"
+L["iLvL ranges"] = "Rangos de iLvl" -- Needs review
+L["Item level settings"] = "Configuraciones de nivel de objeto" -- Needs review
+L["Item settings"] = "Configuraciones de objeto"
+-- L["Itm"] = "Itm"
+-- L["Link raid member balance to pot"] = "Link raid member balance to pot"
+L["Lock"] = "Bloquear"
+-- L["Mail"] = "Mail"
+-- L["Mail money"] = "Mail money"
+L["Manual adjustment"] = "Ajuste manual"
+L["Minimum bid: "] = "Puja minima:"
+L["Minimum increment"] = "Incremento minimo"
+L["Minimum quality"] = "Calidad minima"
+L["Min increment"] = "Incremento min" -- Needs review
+L["Notification options"] = "Opciones de notificación"
+L["Notify outdated versions"] = "Notificar versiones desactualizadas"
+L["Notify outdated versions that are compatible with your version"] = "Notificar versiones desactualizadas que son compatibles con tu versión"
+L["Notify outdated versions that aren't compatible with your version"] = "Notificar versiones desactualizadas que no son compatibles con tu versión"
+L["Notify raid members that do not have GDKPd installed"] = "Notificar miembros de banda que no tienen GDKPd instalado"
+-- L["Per-item settings"] = "Per-item settings"
+-- L["Player balance"] = "Player balance"
+L["Please enter the itemID of an item you want to drop here:"] = "Por favor introduzca el ID de un objeto que quieras soltar aqui:"
+L["Pot export"] = "Exportar bote"
+L["Pot size: %d|cffffd100g|r"] = "Tamaño del bote: %d|cffffd100g|r" -- Needs review
+L["Prevent dragging and hide anchor"] = "Prevenir ancla de arrastre y ocultar" -- Needs review
+L["Request version data"] = "Pedir datos de la versión" -- Needs review
+-- L["Require confirmation when mailing pot shares"] = "Require confirmation when mailing pot shares"
+L["Restart auction"] = "Recomenzar subasta"
+L["Revert highest bid"] = "Revertir la mayor puja"
+L["Rules"] = "Reglas"
+L["Second bidder share"] = "Cuota del segundo postor"
+L["Show"] = "Mostrar"
+L["Show addon frames"] = "Mostrar ventanas del addon"
+-- L["Show auction duration spiral"] = "Show auction duration spiral"
+-- L["Show countdown text on auction duration spiral"] = "Show countdown text on auction duration spiral"
+L["Starting bid"] = "Puja inicial"
+L["Stop bid"] = "Parar puja"
+-- L["The amount of seconds between each announcement of the remaining time"] = "The amount of seconds between each announcement of the remaining time"
+L["The amount of seconds that have to pass after a bid before the auction is closed"] = "La cantidad de segundos que deben pasar tras una puja antes de que se cierre una subasta"
+L["The amount of seconds that have to pass before the auction is closed without bids recieved"] = "La cantidad de segundos que tienen que pasar antes de que la subasta se cierre sin pujas recibidas"
+L["Third bidder share"] = "Cuota del tercer postor"
+L["This player does not have GDKPd running or his version of GDKPd does not yet support version checks."] = "Este jugador no tiene GDKPd funcionando o su versión de GDKPd aún no soporta comprobaciones de versión."
+L["This player has the same version of GDKPd as you do. Full compability is ensured."] = "Este jugador tiene la misma versión de GDKPd que tú. Compatibilidad completa asegurada."
+L["This player's version of GDKPd is more advanced than yours. Please consult your Curse Client for updates or manually check the curse.com page."] = "La versión de GDKPd de este jugador es más avanzada que la tuya. Por favor consulta tu Curse Client por actualizaciones o inspecciona manualmente la pagina curse.com."
+L["This player's version of GDKPd is outdated and one or more functionalities are not compatible:"] = "La versión de GDKPd de este jugador está desactualizado y una o más funcionalidades no son compatibles."
+L["This player's version of GDKPd is outdated. However, their version should be fully compatible with yours."] = "La versión de GDKPd de este jugador está desactualizado. Aun así, su versión debería ser totalmente compatible con la tuya."
+L["This version of GDKPd was never functional due to internal errors."] = "Esta versión de GDKPd nunca fue funcional debido a errores internos."
+L["This version's player balance window will be unable to recognize distributions by you."] = "La ventana de balance de la version de este jugador sera incapaz de reconocer distribuciones realizadas por ti."
+L["This version will be unable to recognize auctions cancelled by you."] = "Esta version será incapaz de reconocer subastas canceladas por ti."
+L["This version will be unable to recognize auctions started by you."] = "Esta versión será incapaz de reconocer las subastas que tu comiences."
+-- L["This version will be unable to recognize version check requests by you. Version check requests sent by this version of GDKPd will not be answered."] = "This version will be unable to recognize version check requests by you. Version check requests sent by this version of GDKPd will not be answered."
+L[ [=[This will completely wipe your auction history and is IRREVERSIBLE.
+Are you completely SURE you want to do this?]=] ] = [=[Esto borrará completamente tu historial de subastas y es IRREVERSIBLE.
+¿Estas completamente SEGURO de que quieres hacer esto?]=]
+-- L["Toggle zero balance"] = "Toggle zero balance"
+-- L["Use looting system loot threshold setting"] = "Use looting system loot threshold setting"
+L["Use slim bidding window even while Master Looter"] = "Usar ventana resumida de pujas incluso siendo Maestro Despojador"
+L["Version notifications"] = "Notificaciones de versión"
+L["Versions"] = "Versiones"
+L["Version status for player %s"] = "Estado de version para jugador %s"
+L["Visibility settings"] = "Configuracion de visibilidad"
+L[ [=[WARNING!
+
+If you use the slim bidding frame, you will be unable to cancel auctions and revert bids!
+Are you certain you want to do this?]=] ] = [=[¡AVISO!
+
+¡Si usas la ventana de pujas resumida, serás incapaz de cancelar subastas y revertir pujas!
+¿Estás seguro de que quieres hacer esto?]=]
+L["Wipe history"] = "Borrar historial"
+L[ [=[You have looted a monster!
+Do you want GDKPd to announce loot?]=] ] = [=[¡Has despojado un monstruo!
+¿Quieres que GDKPd anuncie el loot?]=]
+L[ [=[You have selected the current pot, size %d gold.
+What do you want to do with this pot?]=] ] = [=[Has seleccionado el bote actual, tamaño %s oro.
+¿Qué quieres hacer con este bote?]=] -- Needs review
+L[ [=[You have selected the following pot:
+%s, dated %s, size %d gold.
+What do you want to do with this pot?]=] ] = [=[Has seleccionado el bote siguiente:
+%s, fecha de %s, tamaño %d oro.
+¿Qué quieres hacer con este bote?]=] -- Needs review
